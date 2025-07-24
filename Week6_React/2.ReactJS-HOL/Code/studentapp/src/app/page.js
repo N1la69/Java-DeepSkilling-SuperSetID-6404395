7a34bc1e7190dab7c2fee0fcb1f18fc19427d850
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2025-07-24 19:28:42 +0530

    Week 6 - React - HOL 2 and 3

---
+++ Week6_React/2.ReactJS-HOL/Code/studentapp/src/app/page.js
@@ -0,0 +1,15 @@
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Home from "@/components/Home";
+
+const App = () => {
+  return (
+    <div className="font-bold text-2xl">
+      <Home />
+      <About />
+      <Contact />
+    </div>
+  );
+};
+
+export default App;
---
+++ Week6_React/3.ReactJS-HOL/Code/scorecalculatorapp/src/app/page.js
@@ -0,0 +1,11 @@
+import CalculateScore from "@/components/CalculateScore";
+
+const App = () => {
+  return (
+    <div>
+      <CalculateScore Name={"John"} School={"DPS"} total={567} goal={6} />
+    </div>
+  );
+};
+
+export default App;
